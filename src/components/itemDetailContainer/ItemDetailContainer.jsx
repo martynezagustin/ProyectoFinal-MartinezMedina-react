b7b0973eec8d3e53ebed4f9c49ec7e4a30a0f6e8
Agustín Martínez Medina <martynezagustin@gmail.com>
2023-08-03 15:10:37 -0300

    pushh

--- src/components/itemDetailContainer/ItemDetailContainer.jsx
+++ src/components/itemDetailContainer/ItemDetailContainer.jsx
@@ -3,6 +3,9 @@ import { useFetch } from "../../hooks/useFetch/useFetch";
 import ItemDetail from "../../components/itemDetail/ItemDetail";
 import './itemDetailContainer.css'
 import { API_URLS } from "../../constants";
+import { useEffect } from "react";
+import { collection, doc, getDoc } from "firebase/firestore";
+import { db } from "../../services";
 
 function ItemDetailContainer(){
     const { productId } = useParams()
@@ -11,7 +14,24 @@ function ItemDetailContainer(){
     const history = window.history
     
     const { data } = useFetch(urlProductDetail, API_URLS.PRODUCTS.config)
-    console.log(data);
+
+    useEffect(() => {
+        //Le decimos nuestra database
+        const collectionProducts = collection(db, "products")
+        //hacer una referencia que traiga el id de Param
+        const referenceDoc = doc(collectionProducts, productId)
+        //traer doc
+        getDoc(referenceDoc)
+        .then((result) => {
+            setProductDetail({
+                id: result.id,
+                ...result.data()
+            })
+        })
+        .catch((error) => console.log(error))
+    })
+
+
     return (
         <>
             <div className="headerContainer">
--- src/components/itemListContainer/ItemListContainer.jsx
+++ src/components/itemListContainer/ItemListContainer.jsx
@@ -1,14 +1,40 @@
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { API_URLS } from "../../constants"
 import { useFetch } from "../../hooks/useFetch/useFetch"
 import Item from "../../components/item/Item"
 import { useNavigate, useParams } from "react-router-dom"
 import ItemList from "../itemList/ItemList"
+import { db } from "../../services"
+import { collection, query, where } from "firebase/firestore"
+import { getDocs } from "firebase/firestore"
+
 
 function ItemListContainer() {
-    const { categoryId } = useParams()
     const [cart, setCart] = useState([])
     const { data: products } = useFetch(API_URLS.PRODUCTS.url, API_URLS.PRODUCTS.config)
+    const [productList, setProductList] = useState([])
+    const { categoryId } = useParams()
+    const [loading, setLoading] = useState(false)
+
+
+    //FIREBASE
+    useEffect(()=> {
+        setLoading(true)
+        const productsCollection = categoryId ? query(collection(db, "products"), where("category", "==", categoryId)) : collection(db, "products")
+        getDocs(productsCollection)
+        .then((result) => {
+            const list = result.docs.map((product) => {
+                return({
+                    id:product.id,
+                    ...product.data()
+                }
+                )
+            })
+            setProductList(list)
+        })
+        .catch((error) => {console.log(error)})
+        .finally(setLoading(false))
+    }, [categoryId])
 
     if (categoryId) {
         const productByCategory = products.filter((product) => product.category === categoryId)
@@ -28,7 +54,7 @@ function ItemListContainer() {
                 <h1>Tienda de Beats de Trap, Drill, RKT.</h1>
                 <h2>Urban Diamond Beats</h2>
                 <div className="divProducts">
-                    <ItemList productos={products} />
+                    <ItemList productos={productList} />
                 </div>
             </>
         )
